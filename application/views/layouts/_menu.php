c3ecfa4bcae5132aa9de70bbb13c77ba8d1e186a
ekkyardian <ekkyardian@outlook.com>
2021-03-08 02:27:36 +0700

    :bug: Fix bug: Link menu to Profile and Order

--- application/views/layouts/_menu.php
+++ application/views/layouts/_menu.php
@@ -2,8 +2,8 @@
         <div class="card">
             <div class="card-header">Menu</div>
             <div class="list-group list-group-flush">
-                <li class="list-group-item"><a href="index.php?p=profile">Profile</a></li>
-                <li class="list-group-item"><a href="index.php?p=list-orders">Orders</a></li>
+                <li class="list-group-item"><a href="<?= base_url('profile') ?>">Profile</a></li>
+                <li class="list-group-item"><a href="<?= base_url('order') ?>">Orders</a></li>
             </div>
         </div>
     </div>
